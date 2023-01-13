4cf5fa62757f5b8aa4b3db748d1c2a29e0cf990f
satrio <agungsatrio541@gmail.com>
2023-01-14 00:21:52 +0700

    connect to sql cloud

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 import express from "express";
+import mysql from "mysql";
+
+//import dotenv
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json("Welcome to Google Cloud");
+//database connetion
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
+//router get
+app.get("/", (req, res) => {
+  const query = "SELECT * FROM sambutan";
+  pool.query(query, (error, result) => {
+    if (!result) {
+      res.json({ status: "not found" });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+//app listen
 app.listen(port, () => {
   console.log(`app running on http://localhost:${port}`);
 });
