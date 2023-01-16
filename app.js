afeffba62b25602cefef67fa53e62af4814e7bf5
satrio <agungsatrio541@gmail.com>
2023-01-16 15:52:58 +0700

    deploy simple app to app engine

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ app.use(express.json());
 
 //database connetion
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  // host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
 });
 
 //router get
